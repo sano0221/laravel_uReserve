e547b74e93786c3b708dffde7e351b87d0f814f9
sano0221 <daichiktennis0221@icloud.com>
2022-08-15 17:15:51 +0900

    nst sb

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -1,3 +1,15 @@
 import flatpickr from "flatpickr";
+import { Japanese } from "flatpickr/dist/l10n/ja.js";
 
-flatpickr("#event_date", {});
+
+flatpickr("#event_date", {
+    "locale": Japanese,
+    minDate: "today",
+    maxDate: new Date().fp_incr(30) // 14 days from now
+});
+flatpickr("#start_time", {
+    "locale": Japanese
+});
+flatpickr("#end_time", {
+    "locale": Japanese
+});
--- resources/views/manager/events/index.blade.php
+++ resources/views/manager/events/index.blade.php
@@ -9,20 +9,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <section class="text-gray-600 body-font">
-  <div class="container px-5 py-4 mx-auto">
+    <div class="container px-5 py-4 mx-auto">
     <div class="w-full mx-auto overflow-auto">
-      <table class="table-auto w-full text-left whitespace-no-wrap">
-        <thead>
-          <tr>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">イベント名</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">開始日時</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">終了日時</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">予約人数</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">定員</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">表示・非表示</th>
-            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
-          </tr>
-        </thead>
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+                <tr>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">イベント名</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">開始日時</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">終了日時</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">予約人数</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">定員</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">表示・非表示</th>
+                    <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                </tr>
+            </thead>
         <tbody>
             @foreach($events as $event)
             <tr> 
@@ -35,18 +35,18 @@
             </tr>
             @endforeach
         </tbody>
-      </table>
-      {{ $events->links() }}
+    </table>
+    {{ $events->links() }}
     </div>
     <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
-      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
-      </a>
-      <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+    </a>
+    <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
     </div>
-  </div>
+</div>
 </section>
             </div>
         </div>
