9b58c1013166a9916050bbf54da31a0b53a9f57f
sano0221 <daichiktennis0221@icloud.com>
2022-08-18 16:22:21 +0900

    Services

---
+++ app/Services/EventService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class EventService 
+{
+    public static function checkEventDuplication($eventDate, $startTime, $endTime)
+    {
+        return DB::table('events')
+        ->wheredate('start_date', $eventDate)
+        ->wheretime('end_date', '>',$startTime)
+        ->wheretime('start_date', '<', $endTime)
+        ->exists();
+    }
+
+    public static function joinDateAndTime($date, $time)
+    {
+        $join = $date. " " . $time;
+        return Carbon::createFromFormat('Y-m-d H:i', $join);
+    }
+}
+
