841fa10fdd865d755b349bfa82478074d2c67f04
sano0221 <daichiktennis0221@icloud.com>
2022-08-24 08:53:26 +0900

    baihbgr

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,22 +26,12 @@ class EventController extends Controller
         compact("events"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view("manager.events.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreEventRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(StoreEventRequest $request)
     {
         $check = EventService::checkEventDuplication(
@@ -73,12 +63,6 @@ class EventController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function show(Event $event)
     {
         $event = Event::findOrFail($event->id);
@@ -90,35 +74,50 @@ class EventController extends Controller
         return view('manager.events.show',compact('event', 'eventDate', 'startTime', 'endTime')); 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+      
+        return view('manager.events.edit',compact('event', 'eventDate', 'startTime', 'endTime')); 
+    
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+        $check = EventService::checkEventDuplication(
+            $request['event_date'], $request['start_time'], $request['end_time'] 
+        );
+
+        if($check)
+        {
+            session()->flash('status', 'この時間帯はすでに他の予約でいっぱいです。');
+            return view('manager.events.edit');
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request['start_time']);
+
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request['end_time']);
+
+        
+        $event = Event::findOrFail($event->id);
+        $event->name = $request['event_name'];
+        $event->information = $request['information'];
+        $event->start_date = $startDate;
+        $event->end_date = $endDate;
+        $event->max_people = $request['max_people'];
+        $event->is_visible = $request['is_visible'];
+        $event->save();
+
+        session()->flash('status', '更新しました。');
+
+        return to_route('events.index');
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Event $event)
     {
         //
