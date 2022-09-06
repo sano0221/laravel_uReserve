cba6ebb1443ba81637a16d44fcd5a715cc12f841
sano0221 <daichiktennis0221@icloud.com>
2022-09-07 07:13:38 +0900

    nvaiobvfo

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,6 +1,19 @@
 <div>
-    カレンダー
-    <input id="calendar" class="block mt-1 w-full" type="text" name="calendar" value="{{ $currentDate }}" wire:change="getDate($event.target.value)"/>
+    <div class="text-center text-sm">
+        日付を選択してください。本日から最大30日先まで選択可能です。
+    </div>
+    <input id="calendar" class="block mt-1 mb-2 mx-auto" type="text" name="calendar" value="{{ $currentDate }}" wire:change="getDate($event.target.value)"/>
+    
+    <div class="flex border border-green-400 mx-auto">
+        <x-calendar-time />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+    </div>
     
     <div class="flex">
         @for($day = 0; $day < 7; $day++)
