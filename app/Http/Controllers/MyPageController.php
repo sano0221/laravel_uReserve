989c6dacac760acacc37b9f405d5a5a6737ce35e
sano0221 <daichiktennis0221@icloud.com>
2022-09-15 12:03:31 +0900

    vqeiobviqonpnitb

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Event;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
+use App\Services\MyPageService;
+
 
 class MyPageController extends Controller
 {
     public function index()
     {
-        return view('mypage-index');
+        $user = User::findOrFail(Auth::id());
+        $events = $user->events;
+        $fromTodayEvents = MyPageService::reservedEvent($events, 'fromToday');
+        $pastEvents = MyPageService::reservedEvent($events, 'past');
+        
+
+        // dd($user, $events, $fromTodayEvents, $pastEvents);
+
+        return view('mypage/index', 
+        compact('fromTodayEvents', 'pastEvents'));
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        $reservation = Reservation::where('user_id', '=', Auth::id() )
+        ->where('event_id', '=', $id)
+        ->first();
+
+        return view('mypage/show', compact('event', 'reservation'));
     }
 }
